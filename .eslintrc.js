eff92d9a10a58281189bcd93d2d52b2a033b7c62
thenriquedb <thenrique2012@gmail.com>
2023-02-14 14:48:37 -0300

    config: configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -79,6 +79,7 @@ module.exports = {
     ],
     "class-methods-use-this": "off",
     "import/prefer-default-export": "off",
+    "import/no-extraneous-dependencies": "off",
     "lines-between-class-members": "off",
     "import/extensions": [
       "error",
