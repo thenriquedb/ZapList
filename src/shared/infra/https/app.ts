10c631c183efb7eec4c32dfae7a2e0cdfb3ad567
thenriquedb <thenrique2012@gmail.com>
2023-02-17 16:00:08 -0300

    feat: improve spotify config class

--- src/shared/infra/https/app.ts
+++ src/shared/infra/https/app.ts
@@ -1,9 +1,7 @@
 import express from "express";
 import "express-async-errors";
-import "reflect-metadata";
 
-// import "@shared/infra/dependency-injection";
-
+import { SpotifyWebApi } from "@shared/infra/spotify";
 import { errorHandler } from "@shared/infra/https/middlewares/errorHandler";
 
 import { whatsappRoutes } from "./routes/whatsapp.routes";
@@ -14,60 +12,9 @@ app.use(whatsappRoutes);
 app.use(errorHandler);
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  SpotifyWebApi.authorizationCodeGrant();
+  setInterval(SpotifyWebApi.refreshToken, 1000 * 60 * 59);
+}
+
 app.listen(3000, () => console.log("Server is running"));
-
-// app.get("/", async (req, res) => {
-//   const userPlaylist = await spotifyApi.getUserPlaylists();
-
-//   const changedPlayListId = userPlaylist.body.items.find(
-//     (playlist) => playlist.name === "BOT PLAYLIST"
-//   ).id;
-
-//   const response = await spotifyApi.searchTracks("anavitoria", { limit: 5 });
-
-//   const mappedTracks = response.body.tracks.items.map((track, index) => ({
-//     index,
-//     name: track.name,
-//     id: track.id,
-//     artist: track.artists.map((artist) => artist.name),
-//     uri: track.uri,
-//     duration: track.duration_ms,
-//     album: track.album.name,
-//     preview: track.preview_url,
-//   }));
-
-//   const playlistTracks = await spotifyApi.getPlaylistTracks(changedPlayListId);
-
-//   const musicAlreadyAdded = playlistTracks.body.items.find(
-//     ({ track }) => track.name === mappedTracks[0].name
-//   );
-
-//   if (musicAlreadyAdded) {
-//     return res
-//       .json({
-//         message: "Esta faixa já foi adicionada a playlist e será tocada em breve",
-//       })
-//       .send();
-//   }
-
-//   await spotifyApi.addTracksToPlaylist(changedPlayListId, [mappedTracks[0].uri]);
-
-//   return res.json(mappedTracks).send();
-// });
-
-// app.post("/", async (req, res) => {
-//   const message = req.body.Body;
-//   const tracks = await spotifyApi.searchTracks(message, { limit: 5 });
-//   console.log({ tracks: "" });
-
-//   const twiml = new twilio.twiml.MessagingResponse();
-
-//   const albums = await spotifyApi.getArtistAlbums("43ZHCT0cAZBISjO8DG9PnE", {
-//     limit: 10,
-//     offset: 20,
-//   });
-
-//   twiml.message(JSON.stringify(albums.body.items));
-
-//   return res.send(twiml.toString());
-// });
--- src/shared/infra/spotify/spotify-config.ts
+++ src/shared/infra/spotify/spotify-config.ts
@@ -5,18 +5,72 @@ dotenv.config();
 
 export class SpotifyWebApi {
   private static instance: SpotifyWebApiNode = null;
+  private static tokenExpirationEpoch: number;
+  private static numberOfTimesUpdated: number;
 
-  static getInstance() {
+  static getInstance(): SpotifyWebApiNode {
     if (this.instance == null) {
+      const isProduction = process.env.NODE_ENV === "production";
+      const accessToken = isProduction ? null : process.env.SPOTIFY_ACCESS_TOKEN;
       this.instance = new SpotifyWebApiNode({
-        accessToken: process.env.SPOTIFY_ACCESS_TOKEN,
-        clientId: process.env.SPOTIFY_CLIENT_ID,
-        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-        refreshToken: process.env.SPOTIFY_REFRESH_TOKEN,
+        clientId: "f5f3a3ae89e74b34acb12c215b04169d",
+        clientSecret: "bac3f11a70cc4f448fba2a13c4a63289",
         redirectUri: "http://google.com/",
+        accessToken,
       });
     }
 
     return this.instance;
+  }
+
+  static async authorizationCodeGrant() {
+    const code = process.env.SPOTIFY_CODE;
+
+    try {
+      const data = await this.getInstance().authorizationCodeGrant(code);
+      this.getInstance().setAccessToken(data.body.access_token);
+      this.getInstance().setRefreshToken(data.body.refresh_token);
+
+      // Save the amount of seconds until the access token expired
+      this.tokenExpirationEpoch = new Date().getTime() / 1000 + data.body.expires_in;
+      console.log(
+        `Retrieved token. It expires in ${Math.floor(
+          this.tokenExpirationEpoch - new Date().getTime() / 1000
+        )} seconds!`
+      );
+    } catch (error) {
+      console.log({ error });
+
+      console.log(
+        "Something went wrong when retrieving the access token!",
+        error.message
+      );
+    }
+  }
+
+  static async refreshToken() {
+    console.log(
+      `Time left: ${Math.floor(
+        this.tokenExpirationEpoch - new Date().getTime() / 1000
+      )} seconds left!`
+    );
+
+    // OK, we need to refresh the token. Stop printing and refresh.
+    if (++this.numberOfTimesUpdated > 5) {
+      // Refresh token and print the new time to expiration.
+
+      try {
+        const data = await SpotifyWebApi.getInstance().refreshAccessToken();
+        this.tokenExpirationEpoch = new Date().getTime() / 1000 + data.body.expires_in;
+
+        console.log(
+          `Refreshed token. It now expires in ${Math.floor(
+            this.tokenExpirationEpoch - new Date().getTime() / 1000
+          )} seconds!`
+        );
+      } catch (error) {
+        console.log("Could not refresh the token!", error.message);
+      }
+    }
   }
 }
